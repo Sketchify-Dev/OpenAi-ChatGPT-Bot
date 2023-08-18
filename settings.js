dfff77fc48acd4869d35f3b64c3009d5965951ed
Sketchify <122914026+Sketchify-Dev@users.noreply.github.com>
2023-08-18 08:27:33 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -28,7 +28,7 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 > Ai Bot Password Get From Watching This Video 
 */
 global.AI_PASSWORD = ''
-global.SESSION_ID = ''
+global.SESSION_ID = 'elisa_md@;;;QB1UjCZK#kiNzpYF_md9HD3jEc6MSUSCxwf5CydtSdil0lRwmnYs'
 
 /*
 🎲 𝗢𝗧𝗛𝗘𝗥 𝗦𝗘𝗧𝗜𝗡𝗚𝗦
